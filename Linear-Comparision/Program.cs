fea892455b77aae5bb40a265387e0e0e02aa2779
duvvamounika <mounikaduvva05@gmail.com>
2022-03-31 17:59:13 +0530

    add LineComparisonUC1 branch and program

--- Linear-Comparision/Program.cs
+++ Linear-Comparision/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Linear_Comparision
 {
-    class Program
-    {
+    public class Program
+    { 
         static void Main(string[] args)
         {
+            double x1,y1,x2,y2,length;
+            x1 = 2;
+            y1 = 3;
+            x2 = 4;
+            y2 = 5;
+            length = Math.Sqrt(Math.Abs((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+            Console.WriteLine("length of two points is:{0}",length);
+            Console.ReadLine();
         }
     }
 }
