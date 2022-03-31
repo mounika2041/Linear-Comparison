2b9da198ac3751195463b89d8e6126c0fe27cecd
duvvamounika <mounikaduvva05@gmail.com>
2022-03-31 18:28:31 +0530

    add LineComparisonUC3 branch and program

--- Linear-Comparision/Program.cs
+++ Linear-Comparision/Program.cs
@@ -7,25 +7,29 @@ using System.Threading.Tasks;
 namespace Linear_Comparision
 {
     public class Program
-    { 
+    {
         static void Main(string[] args)
         {
-            double x1,y1,x2,y2,x3,y3,x4,y4,length1,length2;
-            x1 = 2;
-            y1 = 6;
-            x2 = 4;
-            y2 = 9;
+            double x1, y1, x2, y2, x3, y3, x4, y4, length1, length2;
+            x1 = 9;
+            y1 = 12;
+            x2 = 6;
+            y2 = 15;
             x3 = 6;
-            y3 = 3;
-            x4 = 8;
-            y4 = 7;
-            length1 = Math.Sqrt(Math.Abs((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+            y3 = 2;
+            x4 = 9;
+            y4 = 0;
+            length1 = Math.Sqrt(Math.Abs((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
             length2 = Math.Sqrt(Math.Abs((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3)));
-            Console.WriteLine("length of first line is:{0} and second line is:{1}", length1,length2);
+            Console.WriteLine("length of first line is:{0} and second line is:{1}", length1, length2);
+
             if (length1 == length2)
+
                 Console.WriteLine("Two lines are equal");
+            else if (length1 > length2)
+                Console.WriteLine("first line is greater than second line ");
             else
-                Console.WriteLine("Two lines are not equal");
+                Console.WriteLine("first line is less than second line");
             Console.ReadLine();
         }
     }
