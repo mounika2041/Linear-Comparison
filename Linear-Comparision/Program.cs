4dd34dd44865e650796e42270cd9993fa3f57717
duvvamounika <mounikaduvva05@gmail.com>
2022-03-31 18:14:23 +0530

    add LineComparisonUC2 branch and program

--- Linear-Comparision/Program.cs
+++ Linear-Comparision/Program.cs
@@ -10,13 +10,22 @@ namespace Linear_Comparision
     { 
         static void Main(string[] args)
         {
-            double x1,y1,x2,y2,length;
+            double x1,y1,x2,y2,x3,y3,x4,y4,length1,length2;
             x1 = 2;
-            y1 = 3;
+            y1 = 6;
             x2 = 4;
-            y2 = 5;
-            length = Math.Sqrt(Math.Abs((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
-            Console.WriteLine("length of two points is:{0}",length);
+            y2 = 9;
+            x3 = 6;
+            y3 = 3;
+            x4 = 8;
+            y4 = 7;
+            length1 = Math.Sqrt(Math.Abs((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+            length2 = Math.Sqrt(Math.Abs((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3)));
+            Console.WriteLine("length of first line is:{0} and second line is:{1}", length1,length2);
+            if (length1 == length2)
+                Console.WriteLine("Two lines are equal");
+            else
+                Console.WriteLine("Two lines are not equal");
             Console.ReadLine();
         }
     }
